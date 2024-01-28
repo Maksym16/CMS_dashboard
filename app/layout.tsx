6ef19974853820354a2de95f5f09af42e9829b3a
Maksym <mfedorenko16@gmail.com>
2024-01-28 15:13:26 -0500

    added set up for dark light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,41 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
 
-import { ModalProvider } from '@/providers/modal-provide'
-import { ToasterProvider } from '@/providers/toast-provider'
+import { ModalProvider } from '@/providers/modal-provide';
+import { ToasterProvider } from '@/providers/toast-provider';
+import { ThemeProvider } from '@/components/theme-provider';
 
-import './globals.css'
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Admin Dashboard',
   description: 'Admin Dashboard',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ToasterProvider />
-          <ModalProvider />
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ToasterProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,11 @@
-import { UserButton, auth } from "@clerk/nextjs";
-import { MainNav } from "@/components/main-nav";
-import ProjectSwitcher from "@/components/project-switcher";
-import prismadb from "@/lib/prismadb";
-import { redirect } from "next/navigation";
+import { UserButton, auth } from '@clerk/nextjs';
+import { MainNav } from '@/components/main-nav';
+import ProjectSwitcher from '@/components/project-switcher';
+import prismadb from '@/lib/prismadb';
+import { redirect } from 'next/navigation';
+import { ModeToggle } from './ui/mode-toggle';
 
-const Navbar = async() => {
+const Navbar = async () => {
   const { userId } = auth();
 
   if (!userId) {
@@ -12,22 +13,21 @@ const Navbar = async() => {
   }
   const projects = await prismadb.project.findMany({
     where: {
-      userId
-    }
-  })
+      userId,
+    },
+  });
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <ProjectSwitcher items={projects}/>
-        <MainNav 
-          className="px-6"
-        />
+        <ProjectSwitcher items={projects} />
+        <MainNav className="px-6" />
         <div className="ml-auto flex items-center space-x-4">
+          <ModeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
